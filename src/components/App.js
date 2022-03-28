cab59a212ccaae29ca442bcc76cdfea2b9553322
Michael Fournier <michaeljfournier5@gmail.com>
2022-03-28 18:02:17 -0400

    created flow chart, created Favorites component, updated Header component to be Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./Header";
+import Navbar from "./Navbar";
 import CharacterContainer from "./CharacterContainer"
 
 function App() {  
 
   return (
     <div>
-      <Header />
+      <Navbar />
       <CharacterContainer />
     </div>
   );
